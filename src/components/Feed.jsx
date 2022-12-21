9f9e107aafc800518fcd9e91cc290dc74767f6d7
kiplangatmike <m.kiplangat@alustudent.com>
2022-12-21 23:45:42 +0200

    updates

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,25 +12,25 @@ const Feed = () => {
           <div className="font-bold text-red-800 pb-3 pt-1 text-center">
             <h5>News Feed</h5>
           </div>
-          <div className="pl-4 text-red-500 font-bold flex flex-row gap-2 items-center pb-3">
+          <div className="cursor-pointer hover:text-red-500 pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
             <i>
               <FaCompass />
             </i>
             <h4>Browse</h4>
           </div>
-          <div className="pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
+          <div className="cursor-pointer hover:text-red-500 pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
             <i>
               <GiEternalLove />
             </i>
             <h4>Watch list</h4>
           </div>
-          <div className="pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
+          <div className="cursor-pointer hover:text-red-500 pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
             <i>
               <BsCalendar3 />
             </i>
             <h4>Movies</h4>
           </div>
-          <div className="pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
+          <div className="cursor-pointer hover:text-red-500 pl-4 text-white font-bold flex flex-row gap-2 items-center pb-3">
             <i>
               <BsCalendar3 />
             </i>
