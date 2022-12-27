a03a3bbbf138276a67e41c9224a24dfb88b6f4b0
kiplangatmike <m.kiplangat@alustudent.com>
2022-12-27 21:11:00 +0200

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Carousel } from '@trendyol-js/react-carousel';
+import { Carousel } from "@trendyol-js/react-carousel";
+import one from "../assets/images/one.png";
 
 const Hero = () => {
   return (
     <section className="relative bg-witcher bg-center bg-cover ml-4 h-80  drop-shadow-md rounded-2xl">
-      {/* <div className="text-white">
+      <div className="text-white">
         <h2 className="text-xl p-12">
           The{" "}
           <span className="font-bold ">
@@ -12,18 +13,8 @@ const Hero = () => {
             WITCHER
           </span>
         </h2>
-      </div> */}
-      {/* <Carousel show={3.5} slide={3} swiping={true}>
-        <div color="#2d66c3">We love Web 🌐</div>
-        <div color="#f44336">We love Developers 👩🏻‍</div>
-        <a target="_blank" href="https://github.com/trendyol/">
-          <div color="#d53f8c">This is our github</div>
-        </a>
-        <a target="_blank" href="https://trendyol.com/">
-          <div color="#f27a1a">This is our website</div>
-        </a>
-        ...
-      </Carousel> */}
+      </div>
+      <img src={one} alt="logo" className=" absolute ml-80 bottom-6 right-12 w-16 h-10"></img>
     </section>
   );
 };
