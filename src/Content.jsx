21b4243eb228f6113d50c88f0568153da5792fba
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-02 19:30:07 +0200

    updates

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,15 @@
 import witcher from "./assets/images/witcher.jpg";
+import martian from "./assets/images/martian.jpg";
+import avatar from "./assets/images/avatar.jpg";
+import power from "./assets/images/power.jpg";
+import wakanda from "./assets/images/wakanda.jpg";
 
 export const Content = {
-  heroPhotos: { pic: witcher, name: "The Witcher" },
+  heroPhotos: [
+    { pic: witcher, name: "The Witcher" },
+    { pic: martian, name: "The Martian" },
+    { pic: avatar, name: "Avatar" },
+    { pic: power, name: "Power" },
+    { pic: wakanda, name: "Wakanda Forever" },
+  ],
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,47 @@ import React from "react";
 import { Carousel } from "@trendyol-js/react-carousel";
 import one from "../assets/images/one.png";
 import { Content } from "../Content";
-
+import Slider from "react-slick";
+import SingleCardHero from "./SingleCardHero";
 
 const Hero = () => {
-  const {heroPhotos} = Content;
+  const { heroPhotos } = Content;
+
+  const settings = {
+    infinite: true,
+    dots: false,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    lazyLoad: true,
+    // autoplay: true,
+    //   autoplaySpeed: 2000,
+  };
+
   return (
     <section className="relative bg-witcher bg-center bg-cover ml-4 h-80  drop-shadow-md rounded-2xl">
-      <div className="text-white">
-        <h2 className="text-xl p-12">
-          {heroPhotos.name}
-          <span className="font-bold ">
-            <br />
-            WITCHER
-          </span>
-        </h2>
-      </div>
-      <img src={heroPhotos.pic} alt="logo" className=" absolute ml-80 bottom-6 right-12 w-16 h-10"/>
+      
+        <Slider {...settings} className="flex flex-col pl-12 " >
+          {Content.heroPhotos.map((content, i) => (
+            <SingleCardHero key={i} poster={content.pic} title={content.name}>
+              
+            </SingleCardHero>
+          ))}
+        </Slider>
+      
+
+      <h2 className="text-xl p-12">
+        {heroPhotos.name}
+        <span className="font-bold ">
+          <br />
+          WITCHER
+        </span>
+      </h2>
+
+      <img
+        src={heroPhotos.pic}
+        alt="logo"
+        className=" absolute ml-80 bottom-6 right-12 w-16 h-10"
+      />
     </section>
   );
 };
