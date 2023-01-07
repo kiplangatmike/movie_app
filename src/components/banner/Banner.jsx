0802d0e8cdf3b4009b43ed6762756aa312726ba1
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-07 10:56:56 +0200

    updates

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import useSWR from "swr";
-import { SwiperSlide, Swiper} from "swiper/react";
-import SwiperCore, {autoplay} from "swiper";
+import { SwiperSlide, Swiper } from "swiper/react";
+import SwiperCore, { autoplay } from "swiper";
+import { API, fetcher } from "../../config";
+import BannerItem from "./BannerItem";
 
 const Banner = () => {
+  SwiperCore.use([Autoplay]);
+  const { data } = useSWR(API.getMovieList("popular", 1, "movie"), fetcher);
+  const movies = data?.results || [];
+  return (
+    <section className="banner h-[500px] page-container mb-20 overflow-hidden">
+      <Swiper
+        grabCursor="true"
+        slidesPerView={1}
+        autoplay={{ delay: 8000 }}
+      >{movies.length > 0 && movies.slice(0, 10).map((item) => (
+        <SwiperSlide key={item.id}>
+            <BannerItem item={item} mediaType="movie"></BannerItem>
+        </SwiperSlide>
+      ))}</Swiper>
+    </section>
+  );
+};
 
-}
-
-export default Banner; 
+export default Banner;
---
+++ src/components/banner/BannerItem.jsx
@@ -0,0 +1,10 @@
+import { defaultListboxReducer } from "@mui/base"
+import { useNavigate } from "react-router-dom";
+import { useSWR } from "swr";
+import { API, fetcher} from "../../config";
+
+const BannerItem = () => {
+
+}
+
+export default BannerItem;
