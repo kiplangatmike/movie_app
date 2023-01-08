e63483cbd636114f6dfa2344dad4ff2bc8254f46
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-09 00:08:45 +0200

    updates

--- src/utils/RemoveFavorite.jsx
+++ src/utils/RemoveFavorite.jsx
@@ -1,17 +1,35 @@
-import { doc, updateDoc, } from "firebase/firestone";
+import { doc, updateDoc } from "firebase/firestone";
 import { AiFillHeart } from "react-icons/ai";
 import { useAuth } from "../context/auth-context";
 import { db } from "../firebase/firebase.config";
 
 const removefavourite = async (e, idMovie, mediaType, user) => {
-    if (user) {
-        e.preventDefault();
-        const nreListFavorite = user.nreListFavorite.filter(item => item.id !== idMovie)
-        const updateData = doc(db, "users", user?.id);
-        await updateDoc(updateData, {
-            listFavourite: [...newListFavorite],
-        }).then(() => {
-            console.log("Success");
-        });
-    }
-}
+  if (user) {
+    e.preventDefault();
+    const nreListFavorite = user.nreListFavorite.filter(
+      (item) => item.id !== idMovie
+    );
+    const updateData = doc(db, "users", user?.id);
+    await updateDoc(updateData, {
+      listFavourite: [...newListFavorite],
+    }).then(() => {
+      console.log("Success");
+    });
+  } else {
+    console.log("Failed");
+  }
+};
+
+const Heart = ({ id, mediaType, className = "" }) => {
+  const { userInfo } = useAuth();
+  return (
+    <span
+      onClick={(e) => removeFavorite(e, id, mediaType, userInfo)}
+      className={`cursor-pointer ${className}`}
+    >
+        <AiFillHeart></AiFillHeart>
+    </span>
+  );
+};
+
+export default Heart;
