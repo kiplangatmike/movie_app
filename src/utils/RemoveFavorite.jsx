68797e3c2cec4dbaaf741e632c468fc412ecb531
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-08 23:58:24 +0200

    updates

--- src/utils/RemoveFavorite.jsx
+++ src/utils/RemoveFavorite.jsx
@@ -1,1 +1,4 @@
 import { doc, updateDoc, } from "firebase/firestone";
+import { AiFillHeart } from "react-icons/ai";
+import { useAuth } from "../context/auth-context";
+import { db } from "../firebase/firebase.config";
