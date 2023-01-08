86c3d98ea6907afb7438c2d7ac57dd73cb76a561
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-09 00:03:46 +0200

    updates

--- src/utils/RemoveFavorite.jsx
+++ src/utils/RemoveFavorite.jsx
@@ -2,3 +2,16 @@ import { doc, updateDoc, } from "firebase/firestone";
 import { AiFillHeart } from "react-icons/ai";
 import { useAuth } from "../context/auth-context";
 import { db } from "../firebase/firebase.config";
+
+const removefavourite = async (e, idMovie, mediaType, user) => {
+    if (user) {
+        e.preventDefault();
+        const nreListFavorite = user.nreListFavorite.filter(item => item.id !== idMovie)
+        const updateData = doc(db, "users", user?.id);
+        await updateDoc(updateData, {
+            listFavourite: [...newListFavorite],
+        }).then(() => {
+            console.log("Success");
+        });
+    }
+}
