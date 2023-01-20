8a40e748322bb14defe479766cf3102a6f268a62
kiplangatmike <m.kiplangat@alustudent.com>
2023-01-20 23:10:07 +0200

    search update

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -3,20 +3,21 @@ import SingleTrendingCard from "../SingleTrendingCard";
 
 const Series = () => {
   const [genre, setGenre] = useState([]);
-  const []
+  const [numOfPages, setNumOfPages] = useState();
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState([]);
   const genreforURL = useGenre();
 
-  const fetchSeries = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`;
+  const fetchSeries = `https://api.themoviedb.org/3/discover/movie?api_key=a02cb89a765a86c8bf96b049622889fd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`;
 
-  useState(() => {
+  useEffect(() => {
     fetch(fetchSeries)
       .then((response) => response.json())
-      .then((json) => setContent(json));
+      .then((json) => setContent(json))
+    //   .then((json) => setNumOfPages(json));
     window.scroll(0, 0);
-  }, [page]);
+  }, [genreforURL, page]);
   return (
     <div>
       {content &&
